dc5a68634b1c57b27fc31bf3c32aaa504d475975
Nik Gavalas <nikgavalas@gmail.com>
2014-11-27 11:00:16 -0700

    Added some comments

--- Stock Simulator/Stock Simulator/Core/Runnable.cs
+++ Stock Simulator/Stock Simulator/Core/Runnable.cs
@@ -8,5 +8,55 @@ namespace StockSimulator.Core
 {
 	class Runnable : Configurable
 	{
+		/// <summary>
+		/// List of dependent runnables that need to be run before this one.
+		/// </summary>
+		private List<Runnable> _dependents;
+
+		/// <summary>
+		/// Constructor for the runnable.
+		/// </summary>
+		public Runnable() : base()
+		{
+			_dependents = new List<Runnable>();
+		}
+
+		/// <summary>
+		/// Adds another runnable to this runnables dependent list. This will ensure
+		/// that the dependents are run before this one.
+		/// </summary>
+		public void Add(Runnable newDependent)
+		{
+			_dependents.Add(newDependent);
+		}
+
+		/// <summary>
+		/// Initializes the runnable.
+		/// </summary>
+		public virtual void Initialize()
+		{
+		}
+
+		/// <summary>
+		/// Called everytime there is a new bar of data.
+		/// </summary>
+		public virtual void OnBarUpdate()
+		{ 
+		}
+
+		/// <summary>
+		/// Runs this object for all bars included in the simulation.
+		/// </summary>
+		public virtual void Run()
+		{
+		}
+
+		/// <summary>
+		/// Called when the simulation is finished.
+		/// </summary>
+		public virtual void ShutDown()
+		{
+		}
+
 	}
 }
--- Stock Simulator/Stock Simulator/Strategies/Main.cs
+++ Stock Simulator/Stock Simulator/Strategies/Main.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StockSimulator.Core;
 
 namespace StockSimulator.Strategies
 {
-	class Main
+	/// <summary>
+	/// The main strategy that will buy the best deemed stocks each day as long as we have money
+	/// </summary>
+	class Main : Strategy
 	{
+		/// <summary>
+		/// Initializes all the symbols that we are going to consider when buying.
+		/// </summary>
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			// Load the config file with the instument list for all the symbols that we 
+			// want to test.
+
+			// Add all the symbols as dependent strategies using the bestofsubstrategies 
+
+
+		}
+
+		/// <summary>
+		/// Called on each bar update. Will determine which stocks we should buy.
+		/// </summary>
+		public override void OnBarUpdate()
+		{
+			base.OnBarUpdate();
+
+			// Update the broker to sell any shares that need a sellin'
+
+			// For each bar
+			// Get the value of bestof strategy for this bar. 
+			// While we have money left
+			// Get the highest symbol that we don't own
+			// Buy the appropriate amount of shares based on how much money we want to invest per purchase
+		}
 	}
 }
