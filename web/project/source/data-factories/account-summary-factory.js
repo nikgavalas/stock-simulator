24c666483dadb4fe537e5a4a149364550c0f4ec0
Nik Gavalas <nikgavalas@gmail.com>
2015-03-24 07:52:28 -0600

    Added yearly and monthly summary

---
+++ web/project/source/data-factories/account-summary-factory.js
@@ -0,0 +1,112 @@
+'use strict';
+
+mainApp.factory('AccountSummaryFactory', [
+	function(
+	) {
+		var factory = {};
+
+		var monthNames = [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec'
+		];
+
+		/**
+		 * Calculates the yearly and monthly returns of all the values added to the class.
+		 * @param {Number} year Year of the summary
+		 */
+		function YearSummary(year) {
+			this.year = year;
+			this.monthNameToIndex = {};
+			this.months = [];
+		}
+
+		YearSummary.prototype = {
+			/**
+			 * Adds a value to be included in the summary.
+			 * @param {Date} dateOfValue Date of the account value
+			 * @param {Number} value Value of the account on this date
+			 */
+			addValue: function(dateOfValue, value) {
+				var month = parseInt(dateOfValue.getMonth(), 10);
+				var monthName = monthNames[dateOfValue.getMonth()];
+				
+				// Create a new month to hold the values.
+				if (angular.isUndefined(this.monthNameToIndex[monthName])) {
+					this.months.push({
+						name: monthName,
+						startDate: dateOfValue,
+						startValue: value,
+						endDate: dateOfValue,
+						endValue: value,
+						gain: 0,
+						percentGain: 0
+					});
+
+					this.monthNameToIndex[monthName] = this.months.length - 1;
+				}
+
+				// Calculate the gain if this is the new end of the month.
+				var currentMonth = this.months[month];
+				if (dateOfValue > currentMonth.startDate) {
+					currentMonth.endDate = dateOfValue;
+					currentMonth.endValue = value;
+					currentMonth.gain = currentMonth.endValue - currentMonth.startValue;
+					currentMonth.percentGain = ((currentMonth.gain / currentMonth.startValue) * 100.0).toFixed(2);
+				}
+			},
+
+			/**
+			 * Gets the starting value of the year.
+			 * @return {Number} Start value for the year
+			 */
+			getStartValue: function() {
+				return this.months[0].startValue;
+			},
+
+			/**
+			 * Gets the ending value of the year.
+			 * @return {Number} End value for the year
+			 */
+			getEndValue: function() {
+				return this.months[this.months.length - 1].endValue;
+			},
+
+			/**
+			 * Returns the gain for the year.
+			 * @return {Number} The gain for the year
+			 */
+			getGain: function() {
+				return this.getEndValue() - this.getStartValue();
+			},
+
+			/**
+			 * Returns the percentage gain of the year.
+			 * @return {Number} Percent gain for the year
+			 */
+			getPercentGain: function() {
+				return ((this.getGain() / this.getStartValue()) * 100.0).toFixed(2);
+			}
+		};
+
+		/**
+		 * Returns a new year summary object
+		 * @param  {Number} year Year of the year summary
+		 * @return {Object}      YearSummary object
+		 */
+		factory.newYearSummary = function(year) {
+			return new YearSummary(year);
+		};
+
+		return factory;
+	}
+]);
--- web/project/source/page-controllers/account-value-controller.js
+++ web/project/source/page-controllers/account-value-controller.js
@@ -5,18 +5,41 @@ angular.module('mainApp').controller('AccountValueCtrl', [
 	'$routeParams',
 	'$http',
 	'ConfigFactory',
+	'AccountSummaryFactory',
 	function(
 		$scope,
 		$routeParams,
 		$http,
-		ConfigFactory
+		ConfigFactory,
+		AccountSummaryFactory
 	) {
 
 		// Save since it will be used in the rest of the app.
 		ConfigFactory.setOutputFolder($routeParams.runName);
 
+		$scope.yearSummaries = {};
+
 		$http.get(ConfigFactory.getOutputFolder() + 'overall-account.json').success(function(data) {
 			$scope.chartData = data.accountValue;
+
+			var currentYear = new Date($scope.chartData[0][0]).getFullYear();
+			var currentSummary = AccountSummaryFactory.newYearSummary(currentYear);
+			$scope.yearSummaries[currentYear] = currentSummary;
+
+			for (var i = 0; i < $scope.chartData.length; i++) {
+				var timeAndValue = $scope.chartData[i];
+				var dateOfItem = new Date(timeAndValue[0]);
+
+				// Move to the next year if we need to.
+				if (dateOfItem.getFullYear() !== currentYear) {
+					currentYear = dateOfItem.getFullYear();
+					currentSummary = AccountSummaryFactory.newYearSummary(currentYear);
+					$scope.yearSummaries[currentYear] = currentSummary;
+				}
+
+				// Save the value for calculations.
+				currentSummary.addValue(dateOfItem, timeAndValue[1]);
+			}
 		});
 
 	} // end controller
