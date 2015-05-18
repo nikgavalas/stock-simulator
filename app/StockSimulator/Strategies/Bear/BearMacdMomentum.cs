daf25660910d527ce02fd0358a51acca7a95cca1
Nik Gavalas <nikgavalas@gmail.com>
2015-05-18 00:35:56 -0600

    Fixed missing bear strategies

--- app/StockSimulator/Strategies/Bear/BearMacdMomentum.cs
+++ app/StockSimulator/Strategies/Bear/BearMacdMomentum.cs
@@ -14,7 +14,7 @@ namespace StockSimulator.Strategies
 		public BearMacdMomentum(TickerData tickerData, RunnableFactory factory)
 			: base(tickerData, factory)
 		{
-
+			_orderType = Order.OrderType.Short;
 		}
 
 		/// <summary>
