283f78d2c8e8cd76b3b5902de123a54246e57b46
Nik <nikgavalas@gmail.com>
2015-07-05 10:55:14 -0600

    Added Adx indicator to Dmi

---
+++ app/StockSimulator/Indicators/Adx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using StockSimulator.Core;
+
+namespace StockSimulator.Indicators
+{
+	/// <summary>
+	/// Adx
+	/// </summary>
+	class Adx : Indicator
+	{
+		public List<double> Value { get; set; }
+
+		private List<double> dmMinus;
+		private List<double> dmPlus;
+		private List<double> tr;
+		private List<double> sumDmMinus;
+		private List<double> sumDmPlus;
+		private List<double> sumTr;
+
+		#region Configurables
+		public int Period
+		{
+			get { return _period; }
+			set { _period = value; }
+		}
+
+		private int _period = 14;
+		#endregion
+
+
+		public Adx(TickerData tickerData)
+			: base(tickerData)
+		{
+			Value = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			dmMinus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			dmPlus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			tr = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			sumDmMinus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			sumDmPlus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			sumTr = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+		}
+
+		/// <summary>
+		/// Returns the name of this indicator.
+		/// </summary>
+		/// <returns>The name of this indicator</returns>
+		public override string ToString()
+		{
+			return "Adx" + Period.ToString();
+		}
+
+		/// <summary>
+		/// Don't plot on the charts since this is used in the DmiAdx indicator
+		/// </summary>
+		public override bool HasPlot
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Called on every new bar of data.
+		/// </summary>
+		/// <param name="currentBar">The current bar of the simulation</param>
+		public override void OnBarUpdate(int currentBar)
+		{
+			base.OnBarUpdate(currentBar);
+
+			double trueRange = Data.High[currentBar] - Data.Low[currentBar];
+			if (currentBar == 0)
+			{
+				tr[currentBar] = trueRange;
+				dmPlus[currentBar] = 0;
+				dmMinus[currentBar] = 0;
+				sumTr[currentBar] = tr[currentBar];
+				sumDmPlus[currentBar] = dmPlus[currentBar];
+				sumDmMinus[currentBar] = dmMinus[currentBar];
+				Value[currentBar] = 50;
+			}
+			else
+			{
+				tr[currentBar] = Math.Max(Math.Abs(Data.Low[currentBar] - Data.Close[currentBar - 1]), Math.Max(trueRange, Math.Abs(Data.High[currentBar] - Data.Close[currentBar - 1])));
+				dmPlus[currentBar] = Data.High[currentBar] - Data.High[currentBar - 1] > Data.Low[currentBar - 1] - Data.Low[currentBar] ? Math.Max(Data.High[currentBar] - Data.High[currentBar - 1], 0) : 0;
+				dmMinus[currentBar] = Data.Low[currentBar - 1] - Data.Low[currentBar] > Data.High[currentBar] - Data.High[currentBar - 1] ? Math.Max(Data.Low[currentBar - 1] - Data.Low[currentBar], 0) : 0;
+
+				if (currentBar < Period)
+				{
+					sumTr[currentBar] = sumTr[currentBar - 1] + tr[currentBar];
+					sumDmPlus[currentBar] = sumDmPlus[currentBar - 1] + dmPlus[currentBar];
+					sumDmMinus[currentBar] = sumDmMinus[currentBar - 1] + dmMinus[currentBar];
+				}
+				else
+				{
+					sumTr[currentBar] = sumTr[currentBar - 1] - sumTr[currentBar - 1] / Period + tr[currentBar];
+					sumDmPlus[currentBar] = sumDmPlus[currentBar - 1] - sumDmPlus[currentBar - 1] / Period + dmPlus[currentBar];
+					sumDmMinus[currentBar] = sumDmMinus[currentBar - 1] - sumDmMinus[currentBar - 1] / Period + dmMinus[currentBar];
+				}
+
+				double diPlus = 100 * (sumTr[currentBar] == 0 ? 0 : sumDmPlus[currentBar] / sumTr[currentBar]);
+				double diMinus = 100 * (sumTr[currentBar] == 0 ? 0 : sumDmMinus[currentBar] / sumTr[currentBar]);
+				double diff = Math.Abs(diPlus - diMinus);
+				double sum = diPlus + diMinus;
+
+				Value[currentBar] = sum == 0 ? 50 : ((Period - 1) * Value[currentBar - 1] + 100 * diff / sum) / Period;
+			}
+		}
+	}
+}
--- app/StockSimulator/Indicators/DmiAdx.cs
+++ app/StockSimulator/Indicators/DmiAdx.cs
@@ -14,19 +14,40 @@ namespace StockSimulator.Indicators
 	class DmiAdx : Indicator
 	{
 		public List<double> Value { get; set; }
+		public List<double> DmiPlus { get; set; }
+		public List<double> DmiMinus { get; set; }
+		public List<double> Adx { get; set; }
 
-		private int period = 9;
 		private List<double> dmMinus;
 		private List<double> dmPlus;
 		private List<double> tr;
 
+		#region Configurables
+		public int Period
+		{
+			get { return _Period; }
+			set { _Period = value; }
+		}
+
+		private int _Period = 14;
+		#endregion
+	
+		
 		public DmiAdx(TickerData tickerData)
 			: base(tickerData)
 		{
-			Value = Enumerable.Repeat(0d, Data.NumBars).ToList();
-			dmMinus = Enumerable.Repeat(0d, Data.NumBars).ToList();
-			dmPlus = Enumerable.Repeat(0d, Data.NumBars).ToList();
-			tr = Enumerable.Repeat(0d, Data.NumBars).ToList();
+			_dependents = new List<Runnable>()
+			{
+				new Adx(tickerData) { Period = Period }
+			};
+
+			Value = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			DmiPlus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			DmiMinus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			Adx = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			dmMinus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			dmPlus = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
+			tr = UtilityMethods.CreateList<double>(Data.NumBars, 0d);
 		}
 
 		/// <summary>
@@ -35,36 +56,63 @@ namespace StockSimulator.Indicators
 		/// <returns>The name of this indicator</returns>
 		public override string ToString()
 		{
-			return "DmiAdx";
+			return "DmiAdx" + Period.ToString();
 		}
 
 		/// <summary>
-		/// Save the indicator data in a serialization friendly way.
+		/// Creates the plots for the data to be added to.
 		/// </summary>
-		public override void PrepareForSerialization()
+		public override void CreatePlots()
 		{
-			base.PrepareForSerialization();
+			base.CreatePlots();
 
-			// Add the rsi for plotting
-			PlotSeries plot = new PlotSeries("line");
-			ChartPlots[ToString()] = plot;
-			for (int i = 0; i < Data.Dates.Count; i++)
+			// Add the indicator for plotting
+			ChartPlots["Dmi+"] = new PlotSeries("line");
+			ChartPlots["Dmi-"] = new PlotSeries("line");
+			ChartPlots["Adx"] = new PlotSeries("line");
+		}
+
+		/// <summary>
+		/// Adds data to the created plots for the indicator at the current bar.
+		/// </summary>
+		/// <param name="currentBar"></param>
+		public override void AddToPlots(int currentBar)
+		{
+			base.AddToPlots(currentBar);
+
+			long ticks = UtilityMethods.UnixTicks(Data.Dates[currentBar]);
+
+			PlotSeries line = (PlotSeries)ChartPlots["Dmi+"];
+			line.PlotData.Add(new List<object>()
 			{
-				plot.PlotData.Add(new List<object>()
-				{
-					UtilityMethods.UnixTicks(Data.Dates[i]),
-					Math.Round(Value[i], 6)
-				});
-			}
+				ticks,
+				Math.Round(DmiPlus[currentBar], 2)
+			});
+
+			line = (PlotSeries)ChartPlots["Dmi-"];
+			line.PlotData.Add(new List<object>()
+			{
+				ticks,
+				Math.Round(DmiMinus[currentBar], 2)
+			});
+
+			line = (PlotSeries)ChartPlots["Adx"];
+			line.PlotData.Add(new List<object>()
+			{
+				ticks,
+				Math.Round(Adx[currentBar], 2)
+			});
 		}
 
 		/// <summary>
 		/// Called on every new bar of data.
 		/// </summary>
 		/// <param name="currentBar">The current bar of the simulation</param>
-		protected override void OnBarUpdate(int currentBar)
+		public override void OnBarUpdate(int currentBar)
 		{
 			base.OnBarUpdate(currentBar);
+
+			Adx[currentBar] = ((Adx)_dependents[0]).Value[currentBar];
 
 			if (currentBar == 0)
 			{
@@ -72,6 +120,8 @@ namespace StockSimulator.Indicators
 				dmPlus[currentBar] = 0;
 				tr[currentBar] = Data.High[currentBar] - Data.Low[currentBar];
 				Value[currentBar] = 0;
+				DmiPlus[currentBar] = 0;
+				DmiMinus[currentBar] = 0;
 			}
 			else
 			{
@@ -79,9 +129,11 @@ namespace StockSimulator.Indicators
 				dmPlus[currentBar] = Data.High[currentBar] - Data.High[currentBar - 1] > Data.Low[currentBar - 1] - Data.Low[currentBar] ? Math.Max(Data.High[currentBar] - Data.High[currentBar - 1], 0) : 0;
 				tr[currentBar] = Math.Max(Data.High[currentBar] - Data.Low[currentBar], Math.Max(Math.Abs(Data.High[currentBar] - Data.Close[currentBar - 1]), Math.Abs(Data.Low[currentBar] - Data.Close[currentBar - 1])));
 
-				double diPlus = UtilityMethods.Sma(tr, currentBar, period) == 0 ? 0 : UtilityMethods.Sma(dmPlus, currentBar, period) / UtilityMethods.Sma(tr, currentBar, period);
-				double diMinus = UtilityMethods.Sma(tr, currentBar, period) == 0 ? 0 : UtilityMethods.Sma(dmMinus, currentBar, period) / UtilityMethods.Sma(tr, currentBar, period);
+				double diPlus = UtilityMethods.Sma(tr, currentBar, Period) == 0 ? 0 : UtilityMethods.Sma(dmPlus, currentBar, Period) / UtilityMethods.Sma(tr, currentBar, Period);
+				double diMinus = UtilityMethods.Sma(tr, currentBar, Period) == 0 ? 0 : UtilityMethods.Sma(dmMinus, currentBar, Period) / UtilityMethods.Sma(tr, currentBar, Period);
 
+				DmiPlus[currentBar] = diPlus * 100.0;
+				DmiMinus[currentBar] = diMinus * 100.0;
 				Value[currentBar] = (diPlus + diMinus == 0) ? 0 : (diPlus - diMinus) / (diPlus + diMinus);
 			}
 		}
